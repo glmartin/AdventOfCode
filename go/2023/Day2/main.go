690439be934a8ce791b6b74500ccf9a1fa0ef662
Greg Martin <greg.l.martin@gmail.com>
2023-12-28 16:53:38 -0500

    Starting codeing for Day 2

--- go/2023/Day2/main.go
+++ go/2023/Day2/main.go
@@ -1,9 +1,124 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+// Game 77: 3 green, 5 red, 8 blue; 14 red, 15 green; 14 green, 1 blue, 2 red
+type Game struct {
+	ID   int
+	Sets []GameSet
+}
+
+type GameSet struct {
+	Red   int
+	Blue  int
+	Green int
+}
+
+var inputFileName = "input"
+
+func ScanFile(inputFile string) ([]string, error) {
+	// open the inoput file
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// read the file into the slice
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func parseLine(line string) (Game, error) {
+
+	g := Game{}
+
+	var idRegex = regexp.MustCompile("[0-9]+")
+
+	// Game 77: 3 green, 5 red, 8 blue; 14 red, 15 green; 14 green, 1 blue, 2 red
+	lineparts := strings.Split(line, ":")
+
+	fmt.Printf("%s  \n", lineparts[0])
+
+	// parse the game ID
+	gameIdMatches := idRegex.FindSubmatch([]byte(lineparts[0]))
+	fmt.Printf("%s  \n", gameIdMatches)
+	id, err := strconv.Atoi(string(gameIdMatches[0]))
+	if err != nil {
+		return g, err
+	}
+
+	g.ID = id
+
+	// parse the sets
+	gameSets := make([]GameSet, 0)
+	sets := strings.Split(lineparts[1], ";")
+
+	for _, s := range sets {
+
+		gs := GameSet{}
+		setSplit := strings.Split(s, ",")
+
+		for _, setItem := range setSplit {
+
+			var countRegex = regexp.MustCompile("[0-9]+")
+
+			// parse the count
+			fmt.Println(setItem)
+			countMatches := countRegex.FindSubmatch([]byte(setItem))
+			count, err := strconv.Atoi(string(countMatches[0]))
+			if err != nil {
+				return g, err
+			}
+
+			if strings.Contains(setItem, "red") {
+				gs.Red = count
+			} else if strings.Contains(setItem, "blue") {
+				gs.Blue = count
+			} else if strings.Contains(setItem, "green") {
+				gs.Green = count
+			}
+		}
+
+		gameSets = append(gameSets, gs)
+
+	}
+
+	g.Sets = gameSets
+
+	return g, nil
+
+}
+
+func ParseGames(fileContents []string) ([]*Game, error) {
+
+	for _, line := range fileContents {
+		_, err := parseLine(line)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return nil, nil
+}
 
 func main() {
 	log.Println("BEGIN")
+	fileContents, err := ScanFile(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ParseGames(fileContents)
 }
--- go/2023/Day2/main_test.go
+++ go/2023/Day2/main_test.go
@@ -1,19 +1,131 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindResultPart1(t *testing.T) {
-	scanner, err := ScanFile(inputFileName)
+var fileContents []string
+var err error
+
+func TestMain(m *testing.M) {
+	fileContents, err = ScanFile("input")
+	if err != nil {
+		panic(err)
+	}
+
+	exitVal := m.Run()
+
+	os.Exit(exitVal)
+}
+
+func TestScanFile(t *testing.T) {
+	fileContents, err = ScanFile("input")
 	assert.NoError(t, err)
 
-	value, err := FindResultPart1(scanner)
+	assert.Equal(t, 100, len(fileContents))
+}
 
-	assert.NoError(t, err)
+func TestParseLine(t *testing.T) {
 
-	var expected = 53194
-	assert.Equal(t, expected, value)
+	inputMap := map[string]Game{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green": Game{
+			ID: 1,
+			Sets: []GameSet{
+				GameSet{
+					Red:  4,
+					Blue: 3,
+				},
+				GameSet{
+					Red:   1,
+					Blue:  6,
+					Green: 2,
+				},
+				GameSet{
+					Green: 2,
+				},
+			},
+		},
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue": Game{
+			ID: 2,
+			Sets: []GameSet{
+				GameSet{
+					Green: 2,
+					Blue:  1,
+				},
+				GameSet{
+					Red:   1,
+					Blue:  4,
+					Green: 3,
+				},
+				GameSet{
+					Blue:  1,
+					Green: 1,
+				},
+			},
+		},
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red": Game{
+			ID: 3,
+			Sets: []GameSet{
+				GameSet{
+					Red:   20,
+					Green: 8,
+					Blue:  6,
+				},
+				GameSet{
+					Red:   4,
+					Blue:  5,
+					Green: 13,
+				},
+				GameSet{
+					Red:   1,
+					Green: 5,
+				},
+			},
+		},
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red": Game{
+			ID: 4,
+			Sets: []GameSet{
+				GameSet{
+					Red:   3,
+					Green: 1,
+					Blue:  6,
+				},
+				GameSet{
+					Red:   6,
+					Green: 3,
+				},
+				GameSet{
+					Red:   14,
+					Blue:  15,
+					Green: 3,
+				},
+			},
+		},
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green": Game{
+			ID: 5,
+			Sets: []GameSet{
+				GameSet{
+					Red:   6,
+					Green: 3,
+					Blue:  1,
+				},
+				GameSet{
+					Red:   1,
+					Blue:  2,
+					Green: 2,
+				},
+			},
+		},
+	}
+
+	for line, expectedStruct := range inputMap {
+
+		g, err := parseLine(line)
+		assert.NoError(t, err)
+
+		assert.Equal(t, expectedStruct, g)
+	}
 }
