1620d78f269c1103cb217ba5f51f200f4a3a47af
Greg Martin <greg.l.martin@gmail.com>
2024-01-02 09:27:53 -0500

    First attempt at finding the value for Day3

--- go/2023/Day3/main.go
+++ go/2023/Day3/main.go
@@ -4,9 +4,20 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var inputFileName = "2023/Day1/input"
+var inputFileName = "2023/Day3/input"
+var symbols = ""
+
+type PossiblePartNumber struct {
+	Value      string
+	LineNumber int
+	Index      int
+}
 
 func ScanFile(inputFile string) ([]string, error) {
 	// open the inoput file
@@ -25,10 +36,142 @@ func ScanFile(inputFile string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func FindResultPart1(fileContents []string) (int, error) {
-	return 0, nil
+func collectSymbols(fileContents []string) {
+	for _, line := range fileContents {
+		for _, char := range line {
+			if !unicode.IsDigit(char) && char != '.' && !strings.ContainsRune(symbols, char) {
+				symbols += string(char)
+			}
+		}
+	}
 }
 
+func FindResultPart1(fileContents []string) (int, error) {
+	possibleParts := findPossibleNumbers(fileContents)
+	numbers, err := findPartNumbers(fileContents, possibleParts)
+	if err != nil {
+		return 0, err
+	}
+
+	total := 0
+	for _, i := range numbers {
+		total += i
+	}
+	return total, nil
+}
+
+func findPossibleNumbers(fileContents []string) []PossiblePartNumber {
+	var idRegex = regexp.MustCompile("[0-9]+")
+	numbers := make([]PossiblePartNumber, 0)
+	for lineNumber, line := range fileContents {
+
+		numStrs := idRegex.FindAllString(line, -1)
+
+		for _, numStr := range numStrs {
+			// find the index in the string of the number
+			regexStr := numStr + "+"
+			idxRegex := regexp.MustCompile(regexStr)
+			indicies := idxRegex.FindAllStringIndex(line, -1)
+			for _, idx := range indicies {
+				possNum := PossiblePartNumber{
+					LineNumber: lineNumber,
+					Value:      numStr,
+					Index:      idx[0],
+				}
+				numbers = append(numbers, possNum)
+			}
+		}
+	}
+	return numbers
+}
+
+func findPartNumbers(fileContents []string, possibleNumbers []PossiblePartNumber) ([]int, error) {
+	numbers := make([]int, 0)
+
+	// look through all of the possible numbers, and only same the numbers that are adjacent to a symbol (other than period).
+	// the code will need to look to the left, right of the string, as well as the line above and below
+	for _, possNum := range possibleNumbers {
+		line := fileContents[possNum.LineNumber]
+		lineBefore := ""
+		lineAfter := ""
+
+		if possNum.LineNumber > 0 {
+			lineBefore = fileContents[possNum.LineNumber-1]
+		}
+		if possNum.LineNumber < len(fileContents)-1 {
+			lineAfter = fileContents[possNum.LineNumber+1]
+		}
+
+		if isPartNumbers(possNum, line, lineBefore, lineAfter) {
+			partNum, err := strconv.Atoi(possNum.Value)
+			if err != nil {
+				return numbers, err
+			}
+			numbers = append(numbers, partNum)
+		}
+	}
+
+	return numbers, nil
+}
+
+func isPartNumbers(possibleNumber PossiblePartNumber, line string, lineBefore string, lineAfter string) bool {
+
+	lowestIndex := 0
+	highestIndex := len(line)
+
+	if possibleNumber.Index > 0 {
+		lowestIndex = possibleNumber.Index - 1
+	}
+
+	if (possibleNumber.Index + len(possibleNumber.Value)) < len(line) {
+		highestIndex = possibleNumber.Index + len(possibleNumber.Value) + 1
+	}
+
+	if possibleNumber.Index > 0 {
+		// check the character to the left
+		if isSymbol(line[(possibleNumber.Index - 1):possibleNumber.Index]) {
+			return true
+		}
+	}
+
+	if (possibleNumber.Index + len(possibleNumber.Value)) < len(line) {
+		// check the character to the right
+		if isSymbol(line[(possibleNumber.Index + len(possibleNumber.Value)) : (possibleNumber.Index+len(possibleNumber.Value))+1]) {
+			return true
+		}
+	}
+
+	if lineBefore != "" {
+		// check the line above
+		subLine := lineBefore[lowestIndex:highestIndex]
+		if containsSymbol(subLine) {
+			return true
+		}
+	}
+
+	if lineAfter != "" {
+		// check the line above
+		subLine := lineAfter[lowestIndex:highestIndex]
+		if containsSymbol(subLine) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isSymbol(char string) bool {
+	return strings.Contains(symbols, char)
+}
+
+func containsSymbol(val string) bool {
+	for _, char := range val {
+		if strings.ContainsRune(symbols, char) {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	log.Println("**** 2023 - Day 3 ****")
@@ -36,6 +179,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	collectSymbols(fileContents)
 	value := 0
 
 	args := os.Args[1:]
@@ -47,6 +191,9 @@ func main() {
 	if args[0] == "--part1" {
 		log.Println("--- Part 1")
 		value, err = FindResultPart1(fileContents)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
 		log.Println("--- Part 2")
 	}
--- go/2023/Day3/main_test.go
+++ go/2023/Day3/main_test.go
@@ -10,6 +10,72 @@ import (
 var fileContents []string
 var err error
 
+var input = []string{
+	"467..114..",
+	"...*......",
+	"..35..633.",
+	"......#...",
+	"617*......",
+	".....+.58.",
+	"..592.....",
+	"......755.",
+	"...$.*....",
+	".664.598..",
+}
+
+var possiblePartNumbers = []PossiblePartNumber{
+	{
+		Value:      "467",
+		LineNumber: 0,
+		Index:      0,
+	},
+	{
+		Value:      "114",
+		LineNumber: 0,
+		Index:      5,
+	},
+	{
+		Value:      "35",
+		LineNumber: 2,
+		Index:      2,
+	},
+	{
+		Value:      "633",
+		LineNumber: 2,
+		Index:      6,
+	},
+	{
+		Value:      "617",
+		LineNumber: 4,
+		Index:      0,
+	},
+	{
+		Value:      "58",
+		LineNumber: 5,
+		Index:      7,
+	},
+	{
+		Value:      "592",
+		LineNumber: 6,
+		Index:      2,
+	},
+	{
+		Value:      "755",
+		LineNumber: 7,
+		Index:      6,
+	},
+	{
+		Value:      "664",
+		LineNumber: 9,
+		Index:      1,
+	},
+	{
+		Value:      "598",
+		LineNumber: 9,
+		Index:      5,
+	},
+}
+
 func TestMain(m *testing.M) {
 	fileContents, err = ScanFile("input")
 	if err != nil {
@@ -26,4 +92,88 @@ func TestScanFile(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, 140, len(fileContents))
+}
+
+func TestCollectSymbols(t *testing.T) {
+	fileContents, err = ScanFile("input")
+	assert.NoError(t, err)
+
+	collectSymbols(fileContents)
+
+	assert.Equal(t, "*@-+#%=/$&", symbols)
+}
+
+func TestFindPossibleNumbers(t *testing.T) {
+
+	result := findPossibleNumbers(input)
+
+	assert.Equal(t, possiblePartNumbers, result)
+
+}
+
+func TestFindPartNumbers(t *testing.T) {
+
+	collectSymbols(input)
+
+	expectedResult := []int{467, 35, 633, 617, 592, 755, 664, 598}
+
+	result, err := findPartNumbers(input, possiblePartNumbers)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedResult, result)
+}
+
+func TestIsPartNumbers(t *testing.T) {
+
+	possibleNumber := PossiblePartNumber{
+		Value:      "35",
+		LineNumber: 2,
+		Index:      2,
+	}
+	line := input[possibleNumber.LineNumber]
+	lineBefore := input[possibleNumber.LineNumber-1]
+	lineAfter := input[possibleNumber.LineNumber+1]
+
+	result := isPartNumbers(possibleNumber, line, lineBefore, lineAfter)
+
+	assert.Equal(t, true, result)
+}
+
+func TestIsSymbol(t *testing.T) {
+	fileContents, err = ScanFile("input")
+	assert.NoError(t, err)
+
+	collectSymbols(fileContents)
+
+	assert.Equal(t, false, isSymbol("."))
+	assert.Equal(t, true, isSymbol("*"))
+	assert.Equal(t, true, isSymbol("/"))
+	assert.Equal(t, false, isSymbol("5"))
+	assert.Equal(t, true, isSymbol("="))
+	assert.Equal(t, true, isSymbol("#"))
+	assert.Equal(t, true, isSymbol("&"))
+}
+
+func TestContainsSymbol(t *testing.T) {
+	fileContents, err = ScanFile("input")
+	assert.NoError(t, err)
+
+	collectSymbols(fileContents)
+
+	inputMap := map[string]bool{
+		"467..114..": false,
+		"...*......": true,
+		"..35..633.": false,
+		"......#...": true,
+		"617*......": true,
+		".....+.58.": true,
+		"..592.....": false,
+		"......755.": false,
+		"...$.*....": true,
+		".664.598..": false,
+	}
+
+	for line, expectedBool := range inputMap {
+		assert.Equalf(t, expectedBool, containsSymbol(line), "%s is incorrect for %s", expectedBool, line)
+	}
 }
